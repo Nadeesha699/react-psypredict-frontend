dc13139ca7de56c4014238ab1a6bab15da9dc0cd
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-08-09 09:49:29 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <label className="text-black-900 font-bold text-4xl">
-            Insight<span className="text-yellow-300">Board</span>
-          </label>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <label>
+          Psy<span>Predict</span>
+        </label>
+        <label>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam hic
+          unde soluta blanditiis! Minima laboriosam nulla, qui atque illo, sunt
+          perspiciatis doloremque iste consequuntur fuga incidunt saepe.
+          Consequatur, explicabo amet.
+        </label>
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
